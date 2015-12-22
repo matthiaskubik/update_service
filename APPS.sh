45fc1de46e01bf2cf9156865085b2408c113b9ab
Michael Kalantar <kalantar@us.ibm.com>
2015-12-22 18:40:05 +0000

    another way to get list of apps

--- APPS.sh
+++ APPS.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 function groupList() {
-  cf apps | awk -v pattern="${CF_APP}_[0-9]\*" '$1 ~ pattern {print $1}'
+  # cf apps | awk -v pattern="${CF_APP}_[0-9]\*" '$1 ~ pattern {print $1}'
+  cf apps | grep "^${CF_APP}_[0-9]*[[:space:]]" | cut -d' ' -f1
   ## TODO error checking on result of cf apps call
 }
 
