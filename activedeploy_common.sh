35b228ce2561c2ddf429bbdfe7fa26cbe31335bc
Michael Kalantar <kalantar@us.ibm.com>
2016-01-13 16:25:26 +0000

    copywrite notices

--- activedeploy_common.sh
+++ activedeploy_common.sh
@@ -1,5 +1,20 @@
 #/bin/bash
 
+#********************************************************************************
+# Copyright 2016 IBM
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#********************************************************************************
+
 # Convert a string representation of a phase into an integer to be used for comparison purposes.
 # Usage: phase_id phase
 function phase_id () {
